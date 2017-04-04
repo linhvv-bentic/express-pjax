28d676c1be4e9bdbe9123ed053c61aec0d27566a
linhvv-bentic <linhvv@bentic.net>
2017-04-04 15:21:59 +0700

    Update pjax.js

--- pjax.js
+++ pjax.js
@@ -1,18 +1,22 @@
+var platform_module = require("../../models/platform_module");
+
 module.exports = function() {
-  return function(req, res, next) {
+    return function(req, res, next) {
 
-    if (req.header('X-PJAX')) {
-      req.pjax = true;
-    }
+        if (req.header('X-PJAX')) {
+            req.pjax = true;
+        }
 
-    res.renderPjax = function(view, options, fn) {
-      if (req.pjax) {
-        view = 'pjax/' + view;
-      }
+        res.renderPjax = function(view, options, fn) {
+            if (req.pjax) {
+                view = 'modules/' + view;
+            }
+            platform_module.renderNavigation(function(err, result) {
+                options["nav"] = result;
+                res.render(view, options, fn);
+            });
+        };
 
-      res.render(view, options, fn);
+        next();
     };
-
-    next();
-  };
 };
